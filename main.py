717d096b84ad1501e0afb4fa09b09034f3fe85d1
patrick <patrick@nytyler.com>
2024-08-17 17:33:23 -0400

    First version of the app

---
+++ main.py
@@ -0,0 +1,136 @@
+from fasthtml.common import * # pyright: ignore
+from html.parser import HTMLParser
+from dataclasses import dataclass, field
+import subprocess
+import os
+
+TAGS_WITHOUT_ENDS = ["input"]
+PYTHON_KWS = [
+    "False", "None", "True", "and", "as", "assert", "async", "await", "break",
+    "continue", "def", "del", "elif", "else", "except", "finally", "for", "from", "global",
+    "if", "import", "in", "is", "lambda", "nonlocal", "not", "or", "pass", "raise", "return",
+    "try", "while", "with", "yield"
+]
+AUTO_FORMATTER = "ruff format"
+PATH_TO_FILE = os.path.join("out", "fasthtml.py")
+AUTO_FORMATTER_CMD = f"{AUTO_FORMATTER} {PATH_TO_FILE}"
+
+app,rt = fast_app()
+
+@dataclass
+class Node:
+    tag: str
+    parent: "None | Node"
+    attrs: list[tuple[str, str | None]]
+    children: list["Node"] = field(default_factory=list)
+    data: str | None = None
+
+class FastHtmlParser(HTMLParser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # if there are more than one children of this starting
+        #   node they will be put in the the tuple syntax
+        special_tuple = Node(tag="", attrs=[], parent=None)
+        self.head = special_tuple
+        self.current = special_tuple
+        self.fast_html_string = ""
+
+    def handle_starttag(self, tag, attrs):
+        new_node = Node(tag=tag, attrs=attrs, parent=self.current)
+        self.current.children.append(new_node)
+        if tag not in TAGS_WITHOUT_ENDS:
+            self.current = new_node
+            
+
+    def handle_endtag(self, tag): # pyright: ignore
+        if self.current.parent is not None:
+            self.current = self.current.parent
+
+    def handle_data(self, data):
+        data = data.strip()
+        data = None if data == "" else data
+        self.current.data = data
+
+    def generate_fast_html(self) -> None:
+        if len(self.head.children) > 1:
+            self.fast_html_string += "(" # )
+            for child in self.head.children:
+                self.add_node_to_string(child)
+                self.fast_html_string += ","
+            self.fast_html_string += ")"
+        elif len(self.head.children) == 1:
+            self.add_node_to_string(self.head.children[0])
+
+
+    def add_node_to_string(self, node: Node) -> None:
+        py_attr_vals: list[tuple[str, str | None]] = []
+        # tag
+        self.fast_html_string += node.tag.capitalize().replace("-", "_") + "(" # )
+        # data
+        if node.data is not None:
+            self.fast_html_string += f'"{node.data}"'
+            if node.children or node.attrs:
+                self.fast_html_string += ","
+        # other nodes
+        for i, child in enumerate(node.children):
+            self.add_node_to_string(child)
+            last_child = i == len(node.children) - 1
+            if not (last_child and len(node.attrs) == 0):
+                self.fast_html_string += ", "
+
+        # attributes
+        for attr_name, attr_value in node.attrs:
+            if attr_name == "class":
+                py_attr = "cls"
+            elif attr_name == "type":
+                py_attr = "inputmode"
+            else:
+                py_attr = attr_name.replace("-", "_")
+                if py_attr in PYTHON_KWS:
+                    py_attr = "_" + py_attr
+            py_attr_vals.append((py_attr, attr_value))
+        py_attr_sequence = ",".join([f'{py_attr}=None' if py_val is None else f'{py_attr}="{py_val}"' 
+                                      for py_attr, py_val in py_attr_vals])
+        self.fast_html_string += py_attr_sequence
+        # end tag
+        self.fast_html_string += ")"
+
+
+@rt('/')
+def get(): 
+    return Title("Html2FastHtml"), Br(), Div(
+            Form(
+                Textarea(id="html_text", rows="20"),
+                Button("To FastHtml", inputmode="submit"),
+                hx_target="#html_text",
+                hx_swap="outerHTML",
+                hx_post="/toFastHtml",
+                ),
+            cls="container",)
+
+copy_fast_html = '''
+    var textarea = document.getElementById('html_text');
+    textarea.select();
+    textarea.setSelectionRange(0, 99999); // For mobile devices
+    document.execCommand('copy');
+'''
+
+
+@rt('/toFastHtml')
+def post(html_text: str): 
+    html_parser = FastHtmlParser()
+    html_parser.feed(html_text)
+    html_parser.generate_fast_html()
+
+    with open(PATH_TO_FILE, "w") as file:
+        file.write(html_parser.fast_html_string)
+    subprocess.run(AUTO_FORMATTER_CMD, shell=True)
+    with open(PATH_TO_FILE, "r") as file:
+        fast_html_text = file.read()
+    print(html_parser.fast_html_string)
+    return Textarea(
+            fast_html_text,
+            id="html_text",
+            rows="30"), Script(copy_fast_html)
+
+serve()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Html2FastHtml 
+This is small server to convert html to FastHtml components
+# setup
