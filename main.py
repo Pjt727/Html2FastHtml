5ce01b0ed634a818b5e887afa9a7f84b4457938c
patrick <patrick@nytyler.com>
2024-08-17 20:19:58 -0400

    Some edge cases covered and better copy interface

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from dataclasses import dataclass, field
 import subprocess
 import os
 
-TAGS_WITHOUT_ENDS = ["input"]
+TAGS_WITHOUT_ENDS = [ "img", "input", "br", "hr", "meta", "link", "base", "col", "area", "param" ]
 PYTHON_KWS = [
-    "False", "None", "True", "and", "as", "assert", "async", "await", "break",
-    "continue", "def", "del", "elif", "else", "except", "finally", "for", "from", "global",
-    "if", "import", "in", "is", "lambda", "nonlocal", "not", "or", "pass", "raise", "return",
-    "try", "while", "with", "yield"
-]
-AUTO_FORMATTER = "ruff format"
-PATH_TO_FILE = os.path.join("out", "fasthtml.py")
-AUTO_FORMATTER_CMD = f"{AUTO_FORMATTER} {PATH_TO_FILE}"
+        "False", "None", "True", "and", "as", "assert", "async", "await", "break",
+        "continue", "def", "del", "elif", "else", "except", "finally", "for", "from", "global",
+        "if", "import", "in", "is", "lambda", "nonlocal", "not", "or", "pass", "raise", "return",
+        "try", "while", "with", "yield"
+        ]
+# Tags that need to be done through ft_hx
+MANUAL_TAGS = ["path"]
+
+AUTO_FORMATTER = os.environ.get("AUTO_FORMATTER", "ruff format")
+PATH_TO_FILE = os.environ.get("PATH_TO_FILE", os.path.join("out", "fasthtml.py"))
+AUTO_FORMATTER_CMD = os.environ.get("AUTO_FORMATTER_CMD", f"{AUTO_FORMATTER} {PATH_TO_FILE}")
 
 app,rt = fast_app()
 
@@ -43,7 +46,7 @@ class FastHtmlParser(HTMLParser):
             
 
     def handle_endtag(self, tag): # pyright: ignore
-        if self.current.parent is not None:
+        if self.current.parent is not None and (tag not in TAGS_WITHOUT_ENDS):
             self.current = self.current.parent
 
     def handle_data(self, data):
@@ -65,7 +68,12 @@ class FastHtmlParser(HTMLParser):
     def add_node_to_string(self, node: Node) -> None:
         py_attr_vals: list[tuple[str, str | None]] = []
         # tag
-        self.fast_html_string += node.tag.capitalize().replace("-", "_") + "(" # )
+        if node.tag in MANUAL_TAGS:
+            self.fast_html_string += f'ft_hx("{node.tag}"' # )
+            if node.data is not None or node.children or node.attrs:
+                self.fast_html_string += ","
+        else:
+            self.fast_html_string += node.tag.capitalize().replace("-", "_") + "(" # )
         # data
         if node.data is not None:
             self.fast_html_string += f'"{node.data}"'
@@ -77,7 +85,6 @@ class FastHtmlParser(HTMLParser):
             last_child = i == len(node.children) - 1
             if not (last_child and len(node.attrs) == 0):
                 self.fast_html_string += ", "
-
         # attributes
         for attr_name, attr_value in node.attrs:
             if attr_name == "class":
@@ -88,38 +95,120 @@ class FastHtmlParser(HTMLParser):
                 py_attr = attr_name.replace("-", "_")
                 if py_attr in PYTHON_KWS:
                     py_attr = "_" + py_attr
-            py_attr_vals.append((py_attr, attr_value))
-        py_attr_sequence = ",".join([f'{py_attr}=None' if py_val is None else f'{py_attr}="{py_val}"' 
+            if attr_value is None:
+                py_attr_vals.append((py_attr, attr_value))
+                continue
+            single_quotes = "'" in attr_value
+            double_quotes = '"' in attr_value
+            if single_quotes and double_quotes:
+                if attr_value[-1] == "'":
+                    # in the case where the last character is ' it needs to be escaped
+                    modified_string = attr_value[:-1] + "\\" + attr_value[-1]
+                py_value = f"'''{attr_value}'''"
+            elif double_quotes:
+                py_value = f"'{attr_value}'"
+            else:
+                py_value = f'"{attr_value}"'
+
+            py_attr_vals.append((py_attr, py_value))
+        py_attr_sequence = ",".join([f'{py_attr}=None' if py_val is None else f'{py_attr}={py_val}' 
                                       for py_attr, py_val in py_attr_vals])
         self.fast_html_string += py_attr_sequence
         # end tag
         self.fast_html_string += ")"
 
+copy_fast_html = '''
+    function copyHtml() {
+        let textarea = document.getElementById('html_text');
+        textarea.select();
+        textarea.setSelectionRange(0, 99999); // For mobile devices
+        document.execCommand('copy');
+        textarea.setSelectionRange(0, 0);
+    }
+'''
+copy_icon = Svg(
+        ft_hx(
+            "path",
+            d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z",
+            ),
+        ft_hx(
+            "path",
+            d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z",
+            ),
+        onclick="copyHtml()",
+        hx_get="/copyIcon/copied",
+        hx_swap="outerHTML",
+        hx_target="this",
+        style="cursor: pointer; display: inline",
+        xmlns="http://www.w3.org/2000/svg",
+        width="64",
+        height="64",
+        fill="currentColor",
+        cls="bi bi-clipboard",
+        viewbox="0 0 16 16",
+        )
+copied_icon = Svg(
+        ft_hx(
+            "path",
+            fill_rule="evenodd",
+            d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0",
+            ),
+        ft_hx(
+            "path",
+            d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z",
+            ),
+        ft_hx(
+            "path",
+            d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z",
+            ),
+        xmlns="http://www.w3.org/2000/svg",
+        hx_trigger="load delay:500ms",
+        color="green",
+        hx_swap="outerHTML",
+        hx_target="this",
+        hx_get="/copyIcon/copy",
+        width="64",
+        height="64",
+        fill="currentColor",
+        cls="bi bi-clipboard-check",
+        viewbox="0 0 16 16",
+        )
+
+@rt('/copyIcon/{copy_or_copied}')
+def get(copy_or_copied: str): # pyright: ignore
+    return copied_icon if copy_or_copied == "copied" else copy_icon
 
 @rt('/')
 def get(): 
-    return Title("Html2FastHtml"), Br(), Div(
-            Form(
-                Textarea(id="html_text", rows="20"),
-                Button("To FastHtml", inputmode="submit"),
-                hx_target="#html_text",
-                hx_swap="outerHTML",
-                hx_post="/toFastHtml",
+    return (
+            Title("Html2FastHtml"), 
+            Br(),
+            Div(
+                Form(
+                    Textarea(id="html_text", rows="20"),
+                    Button("To FastHtml", _type="submit"),
+                    copy_icon,
+                    Label("Automatic Copy to Clipboard", _for="automaticCopy", style="display: inline"),
+                    Input(id="automatic_copy", _type="checkbox"),
+                    hx_target="#html_text",
+                    hx_swap="outerHTML",
+                    hx_post="/toFastHtml",
+                    ),
+                style="position: relative",
+                cls="container",
                 ),
-            cls="container",)
+            Script(copy_fast_html)
+            )
 
-copy_fast_html = '''
-    var textarea = document.getElementById('html_text');
-    textarea.select();
-    textarea.setSelectionRange(0, 99999); // For mobile devices
-    document.execCommand('copy');
-'''
-
+@dataclass
+class ToFastHtmlRequest:
+    html_text: str
+    automatic_copy: bool = False
 
 @rt('/toFastHtml')
-def post(html_text: str): 
+def post(fast_html_body: ToFastHtmlRequest): 
     html_parser = FastHtmlParser()
-    html_parser.feed(html_text)
+    html_parser.feed(fast_html_body.html_text)
     html_parser.generate_fast_html()
 
     with open(PATH_TO_FILE, "w") as file:
@@ -127,10 +216,10 @@ def post(html_text: str):
     subprocess.run(AUTO_FORMATTER_CMD, shell=True)
     with open(PATH_TO_FILE, "r") as file:
         fast_html_text = file.read()
-    print(html_parser.fast_html_string)
+    script_contents = "copyHtml()" if fast_html_body.automatic_copy else ""
     return Textarea(
             fast_html_text,
             id="html_text",
-            rows="30"), Script(copy_fast_html)
+            rows="20"), Script(script_contents)
 
 serve()
--- readme.md
+++ readme.md
@@ -1,3 +1,17 @@
 # Html2FastHtml 
-This is small server to convert html to FastHtml components
+This is small server to convert html to FastHtml components.
+The web server is made with [FastHtml](https://github.com/AnswerDotAI/fasthtml "fasthtml github") and
+   uses [ruff](https://github.com/astral-sh/ruff "Ruff github") as a code formatter by default.
+It works by taking forming the submitted html into a tree and then traverses that tree
+   generating FastHtml components.
+Once the FastHtml component string is done it writes it to a file and then formats it.
+Any formatter configuration you wish to apply should work like normal.
+# demo
+
 # setup
+- Clone the repository
+- Install dependencies (pip venv example below)
+    - `python -m venv venv` (creates virtual environment)
+    - windows: `.\venv\Scripts\activate` OR Mac/Linux `source venv/bin/activate` (activates the virtual environment)
+    - `pip install -r requirement.txt` (installs required libaries: fasthtml and ruff)
+- run the website locally `python main.py`
